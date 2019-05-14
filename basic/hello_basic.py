1703876e04516ff9b9b7cf83c665b343d3cae3fb
maz_leo <xtleon.xl@gmail.com>
2019-05-15 03:00:32 +0600

    Update hello_basic.py

--- basic/hello_basic.py
+++ basic/hello_basic.py
@@ -1,2 +1,7 @@
 # by this hashtag we comments in python
-print("Hello Python") #this will print Hello python
+#different type of quote's
+print('Hello Python') #this will print Hello python
+print("Double quote's") #inside double we can use single quotes
+print("Leo's Desktop")
+#print('Leo's  "Laptop") this will create a SyantaxError: invalid syntax
+print('Leo\'s "Laptop"') #this will the correct answer
