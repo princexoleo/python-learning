dcf56d63c1031782d1a3ed1561d0901f03713f15
maz_leo <xtleon.xl@gmail.com>
2019-05-27 05:30:44 +0600

    Create variable.py
    - basic variable create
    - type method for class type

---
+++ basic/variable.py
@@ -0,0 +1,14 @@
+a= 10
+b= 5
+print(a+b) #result will print 15 here
+a =10.5
+print(b+a) # now print 15.5 here
+#and varial a is a float type variable
+print(type(a)) #print <class 'float'>
+print(type(b)) #print <class 'int'>
+name = 'String type'
+print(type(name)) # <class 'str'>
+
+#if we want to find any variable class type then
+#we use this type() methods and in type method we pass
+#our desire variable 
