a40b118742c78319f91031562aed2342587eb0b2
maz_leo <xtleon.xl@gmail.com>
2019-05-27 05:48:01 +0600

    Create basic_math.py
    show basic math funtionality

---
+++ basic/basic_math.py
@@ -0,0 +1,24 @@
+#for addition
+print(10+8) #print 18
+
+#for substraction
+print(20-5) #print 15
+
+#for multiplication
+print(10*6) #print 60
+
+#for Division
+print(60/6) #print 10.0
+
+#for modulation
+print(60/11) #print 5.45454545...
+
+#normal pow
+print(2*2*2*2) #print 16
+#special feature in Python
+print(2**4) #print same answer 16
+#2 is the base and 4 is the power
+
+#in string
+a="HeLL"
+print(4*a)
