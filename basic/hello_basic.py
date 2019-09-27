6eddb974f10f9ae9a3a94c3d2e0b5afbcb1e3d47
maz_leo <xtleon.xl@gmail.com>
2019-09-27 18:11:05 +0600

    hello basic python updated

--- basic/hello_basic.py
+++ basic/hello_basic.py
@@ -1,7 +1,11 @@
 # by this hashtag we comments in python
-#different type of quote's
-print('Hello Python') #this will print Hello python
-print("Double quote's") #inside double we can use single quotes
+# different type of quote's
+print('Hello Python')    # this will print Hello python
+print("Double quote's")    # inside double we can use single quotes
 print("Leo's Desktop")
-#print('Leo's  "Laptop") this will create a SyantaxError: invalid syntax
-print('Leo\'s "Laptop"') #this will the correct answer
+# print('Leo's  "Laptop") this will create a SyantaxError: invalid syntax
+print('Leo\'s "Laptop"')     # this will the correct answer
+
+print('o-----')
+print(' |||||')
+print('*' * 10)  # multiplying string by 10
